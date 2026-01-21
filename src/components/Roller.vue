88d023eceb9e0f60b7bf5bcb930253918192ac0f
unknown <j.d.kotlarski@gmx.com>
2026-01-21 15:58:33 +0100

    I quite misunderstood how reactive works and that it is for objects with deep recursive reactivity

--- src/components/Roller.vue
+++ src/components/Roller.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, reactive} from 'vue';
+import {reactive} from 'vue';
 import Dice from './Dice.vue';
 
 // Simple constants
@@ -9,102 +9,124 @@ const diceAmount = 5;
 const normal = '______';
 const invert = 'invert';
 
-const total = ref(0);
+const buttonMessage = 'Opnieuw rollen?';
 
 // reactive() for arrays and simpler handling
-const diceInversions = reactive([]);
 
-// const diceTrolls = defineModel('diceTrolls', {type: Array, default: []});
-const diceRolls = defineModel('diceRolls', {type: Array, default: []});
+const publicDice = defineModel('publicRolls', {type: Array, default: []});
 
-// const diceRolls = ref([]);
-
-// reactive() objects can just be passed along like variables
-const reactArrayIndex = (reactName, index, content) => {
-    // Check whether generating or changing array contents
-    if (index === reactName.length + 1) {
-        // Generate new array contents
-        reactName.push({id: index, entry: content});
-        // reactName.push({id: index + 1, entry: 0});
-    } else if (index > 0 && index < reactName.length + 1) {
-        // Change specific array content
-        reactName[index - 1].entry = content;
+const dicoid = index => {
+    if (index > 0 && index <= diceAmount) {
+        return publicDice.rolled[index - 1];
     }
 };
 
-const normalization = () => {
-    for (let index = 1; index <= diceAmount; ++index) {
-        reactArrayIndex(diceInversions, index, normal);
+const diceArray = reactive({
+    dice: [],
+    clicked: 0,
+});
+
+// Really nested and powerful way of changing reactive objects
+// Feels a bit like passing parameters by reference in C++
+const cuboid = index => {
+    if (index > 0 && index <= diceAmount) {
+        return diceArray.dice[index - 1];
     }
 };
-
-// const reactArrayInvert = (reactName, index) => {
-//     if (index > 0 && index < reactName.length + 1) {
-//         let flipped = reactName[index - 1].inversion;
-
-//         if (flipped === invert) {
-//             flipped = normal;
-//         } else {
-//             flipped = invert;
-//         }
-
-//         reactName[index - 1].inversion = flipped;
-//     }
-// };
 
 // Simple dice roll function
 const roll = () => Math.floor(valueMax * Math.random()) + 1;
 
-// const flip = index => {
-//     reactArrayInvert(diceRolls.value, index);
+// Fill the dice array in the diceArray object with sensible entries
+// Do the same for the publicDice defineModel object
+const diceArrayFilling = () => {
+    for (let index = 1; index <= diceAmount; ++index) {
+        diceArray.dice.push({id: index, rolled: roll(), inversion: normal});
+
+        publicDice.value.push({id: index, rolled: diceArray.dice[index - 1].rolled});
+    }
+};
+
+diceArrayFilling();
+
+const diceRolling = () => {
+    for (let index = 1; index <= diceAmount; ++index) {
+        const cubid = cuboid(index);
+
+        if (cubid.inversion === normal) {
+            cubid.rolled = roll();
+
+            publicDice.value[index - 1].rolled = cubid.rolled;
+        }
+    }
+
+    ++diceArray.clicked;
+};
+
+const flip = index => {
+    if (index > 0 && index <= diceAmount) {
+        if (diceArray.dice[index - 1].inversion === normal) {
+            diceArray.dice[index - 1].inversion = invert;
+        } else {
+            diceArray.dice[index - 1].inversion = normal;
+        }
+    }
+};
+
+// const resum = () => {
+//     total.value = 0;
+
+//     for (let index = 1; index <= diceAmount; ++index) {
+//         total.value += diceRolls[index - 1].entry;
+//     }
 // };
 
-const single = index => {
-    reactArrayIndex(diceRolls.value, index, roll());
-};
+// const single = index => {
+//     if (index > 0 && index < reactName.length + 1) {
+//         diceInversions[index - 1].entry = diceInversions[index - 1].entry === normal ? invert : normal;
 
-const resum = () => {
-    total.value = 0;
+//         ++clicked;
+//     }
+// };
 
-    for (let index = 1; index <= diceAmount; ++index) {
-        total.value += diceRolls.value[index - 1].entry;
-    }
-};
+// @click="single(rolled.id)"
 
-// Roll all dice, then update results
-const rolling = () => {
-    for (let index = 1; index <= diceAmount; ++index) {
-        reactArrayIndex(diceRolls.value, index, roll());
-    }
+/*
+<div>
+        <Dice
+            @click="single(rolled.id)"
+            v-for="rolled in cubes.rolls"
+            :key="rolled.id"
+            v-model:eyeValue="rolled.entry"
+            :class="cubes.inversions[rolled.id].entry"
+            :inverted="cubes.inversions[rolled.id].entry"
+        />
+    </div>
+    {{ cubes.inversions }}
 
-    resum();
-};
-
-// Roll the dice
-normalization();
-rolling();
+    {{ cubes.clicked }}
+    */
 </script>
 
 <template>
     <div>
         <Dice
-            @click="single(rolled.id)"
-            v-for="rolled in diceRolls"
-            :key="rolled.id"
-            v-model:eyeValue="rolled.entry"
-            :class="normal"
-            :inverted="normal"
+            @click="flip(cube.id)"
+            v-for="cube in diceArray.dice"
+            :key="cube.id"
+            v-model:eyeValue="cube.rolled"
+            :class="cube.inversion"
+            :inverted="cube.inversion"
         />
     </div>
-
-    {{ diceRolls }}
-    {{ diceInversions }}
-
+    {{ diceArray.dice }}
     <br />
-    <button @click="rolling">Opnieuw rollen?</button>
+    <button @click="diceRolling">{{ buttonMessage }}</button>
     <br />
     <div>
-        {{ total }}
+        {{ publicDice }}
+        <br />
+        {{ diceArray.clicked }}
     </div>
     <br />
 </template>
